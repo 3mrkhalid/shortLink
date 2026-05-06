0e15608a00e3a88f385416464303122586bc4f32
3mrkhalid <amrahmedyoussef100@gmail.com>
2026-05-07 00:18:18 +0300

    Refactor MongoDB connection handling
    Refactor DB connection to use mongo.Connect and add ping check.

--- backend-gin/config/dbConn.go
+++ backend-gin/config/dbConn.go
@@ -1,6 +1,5 @@
 package config
 
-
 import (
 	"context"
 	"log"
@@ -13,21 +12,20 @@ import (
 var DB *mongo.Database
 
 func DBConn() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	err = client.Connect(ctx)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	
-	DB = client.Database("syndibook")
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB not reachable:", err)
+	}
+
+	DB = client.Database("ShortLink")
 
 	log.Println("MongoDB Connected")
 }
